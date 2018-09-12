9952fec2dd2e071c377e2019f20ff85380754a15
vipinr <vipinr@houm.me>
2018-09-12 17:33:31 +0530

    Testing a custom auth module

---
+++ src/ejabberd_auth_meerkat.erl
@@ -0,0 +1,65 @@
+%%%-------------------------------------------------------------------
+%%% File    : ejabberd_auth_pam.erl
+%%% Author  : Evgeniy Khramtsov <xram@jabber.ru>
+%%% Purpose : PAM authentication
+%%% Created : 5 Jul 2007 by Evgeniy Khramtsov <xram@jabber.ru>
+%%%
+%%%
+%%% ejabberd, Copyright (C) 2002-2018   ProcessOne
+%%%
+%%% This program is free software; you can redistribute it and/or
+%%% modify it under the terms of the GNU General Public License as
+%%% published by the Free Software Foundation; either version 2 of the
+%%% License, or (at your option) any later version.
+%%%
+%%% This program is distributed in the hope that it will be useful,
+%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%%% General Public License for more details.
+%%%
+%%% You should have received a copy of the GNU General Public License along
+%%% with this program; if not, write to the Free Software Foundation, Inc.,
+%%% 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+%%%
+%%%-------------------------------------------------------------------
+-module(ejabberd_auth_meerkat).
+
+-behaviour(ejabberd_config).
+
+-author('vipinr@gramini.com').
+
+-behaviour(ejabberd_auth).
+
+-export([start/1, stop/1, check_password/4,
+	 user_exists/2, store_type/1, plain_password_required/1,
+	 opt_type/1]).
+
+start(_Host) ->
+    ok.
+
+stop(_Host) ->
+    ok.
+
+check_password(User, AuthzId, Host, Password) ->
+    if AuthzId /= <<>> andalso AuthzId /= User ->
+        false
+    end.
+
+user_exists(User, Host) -> false.
+
+plain_password_required(_) -> true.
+
+store_type(_) -> external.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+-spec opt_type(pam_service) -> fun((binary()) -> binary());
+	      (pam_userinfotype) -> fun((username | jid) -> username | jid);
+	      (atom()) -> [atom()].
+opt_type(pam_service) -> fun iolist_to_binary/1;
+opt_type(pam_userinfotype) ->
+    fun (username) -> username;
+	(jid) -> jid
+    end;
+opt_type(_) -> [pam_service, pam_userinfotype].
